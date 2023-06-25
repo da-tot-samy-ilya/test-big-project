21f973593a4b5a44f591231ba11e2873efc98b49
Ilya Bannikov <da.tot.samy.ilya@gmail.com>
2023-06-25 17:36:10 +0500

    decomposition of webpack.config.ts

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,11 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+import webpack from "webpack";
+import {BuildOptions} from "./types/config";
+
+export const buildPlugins = ({paths}: BuildOptions): webpack.WebpackPluginInstance[] => [
+        new HtmlWebpackPlugin({
+            template: paths.html
+        }),
+        new webpack.ProgressPlugin(),
+    ]
