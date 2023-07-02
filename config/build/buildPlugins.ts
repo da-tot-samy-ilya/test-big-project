be7f3518687d239d930bf5f77d697f4998b45f4c
Ilya Bannikov <da.tot.samy.ilya@gmail.com>
2023-07-02 14:09:57 +0500

    fix devPlugins for actions

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,19 +4,27 @@ import { type BuildOptions } from './types/config'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
-export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] => [
-    new HtmlWebpackPlugin({
-        template: paths.html
-    }),
-    new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin({
-        filename: 'css/[name].[contenthash].css',
-        chunkFilename: 'css/[name].[contenthash].css'
-    }),
-    new webpack.DefinePlugin({
-        _IS_DEV_: JSON.stringify(isDev)
-    }),
-    new BundleAnalyzerPlugin({
-        openAnalyzer: false
-    })
-]
+export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] => {
+    const plugins = [
+        new HtmlWebpackPlugin({
+            template: paths.html
+        }),
+        new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash].css',
+            chunkFilename: 'css/[name].[contenthash].css'
+        }),
+        new webpack.DefinePlugin({
+            _IS_DEV_: JSON.stringify(isDev)
+        })
+
+    ]
+    if (isDev) {
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false
+        }))
+    }
+    return plugins
+}
+
+
